186036c6aae55b2c8bb1a98e1e37134aa6ce358a
FirdausyTama <atama.firdausy@gmail.com>
2023-05-18 17:51:14 +0700

    memberi metode untuk memasukan nilai ke dalam variabel nim dan nama

--- 021_singlelinkedlist/021_singlelinkedlist.cpp
+++ 021_singlelinkedlist/021_singlelinkedlist.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
-usng namespace std;
+using namespace std;
 
 struct Node {
 	int noMhs;
-	string nama;
+	string name;
 	Node* next;
 };
 
 Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukan NIM: ";
+	cin >> nim;
+	cout << "Masukan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+}
